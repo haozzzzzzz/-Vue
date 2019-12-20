744b3032b8ea2033cfba91338d294b9e522dd4ce
zhanghao <zhlovestt@163.com>
2019-12-20 23:54:49 +0800

    完善登录页基本样式

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -4,15 +4,15 @@
       <h1 class="myTitle">专属后台管理系统</h1>
       <el-form :label-position="top" label-suffix=':' label-width='80px'>
         <el-form-item label="账号">
-          <el-input v-model="formLabelAlign.userName"></el-input>
+          <el-input v-model="formLabelAlign.userName" clearable></el-input>
         </el-form-item>
         <el-form-item label="密码">
-          <el-input v-model="formLabelAlign.passWord" type='password'></el-input>
+          <el-input v-model="formLabelAlign.passWord" clearable type='password'> </el-input>
         </el-form-item>
       </el-form>
-      <div>
-        <el-button type="success">成功按钮</el-button>
-        <el-button type="info">信息按钮</el-button>
+      <div class="myBtns">
+        <el-button type="success" size="medium">确认</el-button>
+        <el-button type="info" size="mini">重置</el-button>
       </div>
     </div>
   </div>
@@ -33,6 +33,6 @@ export default {
 </script>
 <style lang="less" scoped>
   .myTitle{
-    color:azure;
+    color:burlywood;
   }
 </style>
